61a884bb72e4a329446998fc1d90d8f33950e103
Elise Phan <aliceconnic@gmail.com>
2019-01-25 05:45:18 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
   def self.current_user(session) #This class method will take one arguement of the user's session
-    @user = User.find_by_id(session[:user_id]) #T
-  end
+    @user = User.find_by_id(session[:user_id]) #This will set an instance variable equal to 
+  end                                          # the value of the user_id in the hash session
   
   def self.is_logged_in?(session)
     !!session[:user_id]
