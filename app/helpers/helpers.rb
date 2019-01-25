ccc1fe0f307d20ae1fe87e346ed977f68b38609f
Elise Phan <aliceconnic@gmail.com>
2019-01-25 05:44:05 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
-  def self.current_user(session)
-    @user = User.find_by_id(session[:user_id])
+  def self.current_user(session) #This class method will take one arguement of the user's session
+    @user = User.find_by_id(session[:user_id]) #T
   end
   
   def self.is_logged_in?(session)
